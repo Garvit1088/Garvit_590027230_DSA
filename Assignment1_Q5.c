67c87dda921d0670f4dc3bb40c0e96e193834d02
Garvit1088 <jgarvit60@gmail.com>
2026-02-10 23:25:49 +0530

    Create Assignment1_Q5.c

---
+++ Assignment1_Q5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Student {
+    int roll;
+    char name[50];
+    float marks;
+};
+
+int main() {
+    int n, i;
+    struct Student *s;
+
+    printf("Enter number of students: ");
+    scanf("%d", &n);
+
+    s = (struct Student*)malloc(n * sizeof(struct Student));
+
+    if (s == NULL) {
+        printf("Memory allocation failed");
+        return 1;
+    }
+
+    for (i = 0; i < n; i++) {
+        printf("\nStudent %d\n", i + 1);
+        printf("Roll No: ");
+        scanf("%d", &s[i].roll);
+        printf("Name: ");
+        scanf(" %[^\n]", s[i].name);
+        printf("Marks: ");
+        scanf("%f", &s[i].marks);
+    }
+
+    printf("\nStudent Details:\n");
+    for (i = 0; i < n; i++) {
+        printf("Roll No: %d\n", s[i].roll);
+        printf("Name: %s\n", s[i].name);
+        printf("Marks: %.2f\n\n", s[i].marks);
+    }
+
+    free(s);
+    return 0;
+}
